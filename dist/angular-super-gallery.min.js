f02fe2149a245e4b13f4982f0e828edf33355662
Tamas Schalk <git@schalk.hu>
2017-08-03 23:52:00 +0200

    version update

--- dist/angular-super-gallery.min.js
+++ dist/angular-super-gallery.min.js
@@ -1,7 +1,7 @@
 /**
  * angular-super-gallery - Super Angular image gallery
  * 
- * @version v0.1.7
+ * @version v0.1.8
  * @link https://github.com/schalkt/angular-super-gallery
  * @license MIT
  */
