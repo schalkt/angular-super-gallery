978c64859965ec2bcdd4f49bd127d74f6a98370b
Tamas Schalk <git@schalk.hu>
2017-11-22 20:58:46 +0100

    Added screenfull to index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('./dist/angular-super-gallery.js');
 var ngAnimate = require('angular-animate');
 var ngTouch = require('angular-touch');
+var screenfull = require('screenfull');
 module.exports = 'angularSuperGallery';
