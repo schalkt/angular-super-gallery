2408a6173403364f867a227dd39028bfaba1e7d9
Tamas Schalk <git@schalk.hu>
2018-02-14 22:00:24 +0100

    Version update

--- dist/angular-super-gallery.js
+++ dist/angular-super-gallery.js
@@ -1,7 +1,7 @@
 /**
  * angular-super-gallery - AngularJS Super Gallery
  * 
- * @version v1.2.1
+ * @version v1.2.2
  * @link http://schalk.hu/projects/angular-super-gallery/demo/
  * @license MIT
  */
