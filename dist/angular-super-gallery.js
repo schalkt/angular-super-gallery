a1b9ef4d010f01c3515fad3c6e4c8706885c8f31
Tamas Schalk <git@schalk.hu>
2019-05-30 22:44:28 +0200

    Version update

--- dist/angular-super-gallery.js
+++ dist/angular-super-gallery.js
@@ -1,7 +1,7 @@
 /**
  * angular-super-gallery - AngularJS Super Gallery
  * 
- * @version v2.0.3
+ * @version v2.0.4
  * @link http://schalk.hu/projects/angular-super-gallery/demo/
  * @license MIT
  */
@@ -584,7 +584,7 @@ var angularSuperGallery;
             this.location = location;
             this.$rootScope = $rootScope;
             this.$window = $window;
-            this.version = "2.0.3";
+            this.version = "2.0.4";
             this.slug = 'asg';
             this.items = [];
             this.files = [];
